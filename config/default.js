19acc5d3a40e92b58656c53b8987b14ab8a12567
Konstantin Aleksandrov <aleksandrovkm@gmail.com>
2018-11-01 14:19:11 +0300

    Move nats connection config

--- config/default.js
+++ config/default.js
@@ -7,14 +7,27 @@ module.exports = {
     logRoot: process.env.LOG_PATH || join(process.cwd(), './logs'),
     secret: ['technolab'],
     mongoose: {
+        uri: 'mongodb://mongo:27017/technolab',
         dbName: 'technolab',
         options: {
-            keepAlive: 1,
-            poolSize: 5,
+            user: 'technolab',
+            pass: '647hrtT5jcTuryj678xksKEfk2psJ790vsauT',
+            autoIndex: false,
+            keepAlive: 120,
+            poolSize: 10,
             connectTimeoutMS: 0,
             socketTimeoutMS: 0,
             promiseLibrary: global.Promise,
-            reconnectTries: Number.MAX_VALUE
+            reconnectTries: Number.MAX_VALUE,
+            bufferMaxEntries: 0
         }
+    },
+    nats: {
+        url: 'nats://localhost:4222',
+        encoding: 'utf8',
+        json: false,
+        maxReconnectAttempts: 10,
+        reconnect: true,
+        reconnectTimeWait: 1000
     }
 };
